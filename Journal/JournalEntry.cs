29064602b4f8ccffc495e19ec55a935e551b7ce3
Jonathan Allen <jonathan.allen@live.com>
2017-10-12 22:46:01 -0600

    Initial commit

---
+++ Journal/JournalEntry.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Journal
+{
+    public class JournalEntry : INotifyPropertyChanged
+    {
+        public JournalEntry()
+        {
+            Items = new ObservableCollection<Item>();
+        }
+        public JournalEntry(DateTime date) : this()
+        {
+            Date = date;
+        }
+
+        private DateTime date;
+
+        public DateTime Date
+        {
+            get { return date; }
+            set { date = value; PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Date))); }
+        }
+
+        public ObservableCollection<Item> Items { get; private set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+
+    public class Item
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+    }
+}
---
+++ Journal/ViewModel/MainViewModel.cs
@@ -0,0 +1,84 @@
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.CommandWpf;
+using System;
+using System.Linq;
+using System.Collections.ObjectModel;
+using System.IO;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace Journal.ViewModel
+{
+    public class MainViewModel : ViewModelBase
+    {
+        private string path = "journalEntries.json";
+
+        /// <summary>
+        /// Initializes a new instance of the MainViewModel class.
+        /// </summary>
+        public MainViewModel()
+        {
+            ////if (IsInDesignMode)
+            ////{
+            ////    // Code runs in Blend --> create design time data.
+            ////}
+            ////else
+            ////{
+            ////    // Code runs "for real"
+            ////}
+
+            Entries = new ObservableCollection<JournalEntry>();
+        }
+
+        private RelayCommand newEntry;
+        public RelayCommand NewEntry => newEntry ?? (newEntry = new RelayCommand(() =>
+        {
+            var entry = new JournalEntry(DateTime.Today);
+            setDefaultItems(entry);
+            Entries.Insert(0, entry);
+            SelectedEntry = entry;
+        }));
+
+        private void setDefaultItems(JournalEntry entry)
+        {
+            var mostRecent = Entries.OrderByDescending(e => e.Date).FirstOrDefault();
+            if (mostRecent == null)
+                return;
+
+            foreach (var item in mostRecent.Items)
+            {
+                entry.Items.Add(new Item { Title = item.Title });
+            }
+        }
+
+        private JournalEntry selectedEntry;
+
+        public JournalEntry SelectedEntry
+        {
+            get { return selectedEntry; }
+            set { selectedEntry = value; RaisePropertyChanged(nameof(SelectedEntry)); }
+        }
+
+        public ObservableCollection<JournalEntry> Entries { get; private set; }
+
+        private RelayCommand loaded;
+        public RelayCommand Loaded => loaded ?? (loaded = new RelayCommand(() =>
+        {
+        if (File.Exists(path) && !Entries.Any())
+            {
+                string text = File.ReadAllText(path);
+                var entries = JsonConvert.DeserializeObject<IEnumerable<JournalEntry>>(text);
+                Entries = new ObservableCollection<JournalEntry>( entries);
+                RaisePropertyChanged(nameof(Entries));
+            }
+        }));
+
+        private RelayCommand closing;
+        public RelayCommand Closing => closing ?? (closing = new RelayCommand(() =>
+        {
+            var json = JsonConvert.SerializeObject(Entries.OrderByDescending(e => e.Date), Formatting.Indented);
+            File.WriteAllText(path, json);
+        }));
+
+    }
+}
